2b375a7bafbe9ce8a1e3a7ac095a34d4c6a96cfb
Santiago Ballerini <sballerini@itba.edu.ar>
2023-07-07 18:26:16 -0300

    fix: fix config tests

---
+++ src/config/test.rs
@@ -0,0 +1,100 @@
+use std::ffi::OsString;
+
+use super::*;
+
+#[test]
+fn test_config_build() {
+    let args = vec![
+        OsString::from("-p"),
+        OsString::from("8889"),
+        OsString::from("-t"),
+        OsString::from("hola"),
+        OsString::from("adduser"),
+        OsString::from("user"),
+        OsString::from("pass"),
+    ];
+
+    let args = Arguments::from_vec(args);
+
+    let config = Config::build(args, String::from("hola")).unwrap();
+
+    assert_eq!(config.port, 8889);
+    assert_eq!(config.auth_token, "hola");
+    assert_eq!(
+        config.command,
+        Command::AddUser {
+            name: OsString::from("user"),
+            password: OsString::from("pass")
+        }
+    );
+}
+
+#[test]
+fn test_config_build_no_port() {
+    let args = vec![
+        OsString::from("-t"),
+        OsString::from("hola"),
+        OsString::from("adduser"),
+        OsString::from("user"),
+        OsString::from("pass"),
+    ];
+    let args = Arguments::from_vec(args);
+
+    let config = Config::build(args, String::from("hola")).unwrap();
+
+    assert_eq!(config.port, DEFAULT_PORT);
+    assert_eq!(config.auth_token, "hola");
+    assert_eq!(
+        config.command,
+        Command::AddUser {
+            name: OsString::from("user"),
+            password: OsString::from("pass")
+        }
+    );
+}
+
+#[test]
+fn test_config_build_no_auth_token() {
+    let args = vec![
+        OsString::from("-p"),
+        OsString::from("8889"),
+        OsString::from("adduser"),
+        OsString::from("user"),
+        OsString::from("pass"),
+    ];
+    let args = Arguments::from_vec(args);
+
+    let config = Config::build(args, String::from("hola")).unwrap();
+
+    assert_eq!(config.port, 8889);
+    assert_eq!(config.auth_token, "hola");
+    assert_eq!(
+        config.command,
+        Command::AddUser {
+            name: OsString::from("user"),
+            password: OsString::from("pass")
+        }
+    );
+}
+
+#[test]
+fn test_config_build_no_port_no_auth_token() {
+    let args = vec![
+        OsString::from("adduser"),
+        OsString::from("user"),
+        OsString::from("pass"),
+    ];
+    let args = Arguments::from_vec(args);
+
+    let config = Config::build(args, String::from("hola")).unwrap();
+
+    assert_eq!(config.port, DEFAULT_PORT);
+    assert_eq!(config.auth_token, "hola");
+    assert_eq!(
+        config.command,
+        Command::AddUser {
+            name: OsString::from("user"),
+            password: OsString::from("pass")
+        }
+    );
+}
